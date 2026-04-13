ac7ea3861cbfa327bf0554be06d8418c62eaa089
Neville Brem <contact@nevillebrem.com>
2026-04-13 15:18:33 +0200

    init with test model

---
+++ src/lib/Scene.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { T, useTask } from '@threlte/core';
+	import { interactivity } from '@threlte/extras';
+
+	import Mac from './Mac.svelte';
+
+	interactivity();
+
+	let rotation = $state(0);
+	useTask((delta) => {
+		rotation += delta * 0.5;
+	});
+</script>
+
+<T.DirectionalLight position={[0, 10, 10]} />
+
+<T.PerspectiveCamera
+	makeDefault
+	position={[10, 10, 10]}
+	oncreate={(ref) => {
+		ref.lookAt(0, 1, 0);
+	}}
+	zoom={5}
+/>
+
+<Mac rotation.y={rotation} />
